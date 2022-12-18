10fbd4b6e4dfcb2a74258c3e976859e8c9ccd22b
khaivern <khaivern@agmostudio.com>
2022-12-18 19:02:07 +0800

    End of section 3

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+    users: any = [];
+
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+        this.http.get('https://localhost:5001/api/users').subscribe(res => (this.users = res));
+    }
+}
